85a4968550418939c6c2971e9ce50e740283dcc3
Reshma057 <reshuu025@gmail.com>
2026-02-22 12:57:22 +0545

    Setup Express with dotenv

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log("Hello World")
+const express = require("express");
+const dotenv = required("dotenv");
+dotenv.config();
+
+const app = express();
+
+const PORT = process.env.PORT;
+
+app.listen(PORT, () => {
+console.log(`Server is runnong on PORT ${PORT}`)
+} );
+
+app.get("/", (req, res) => {
+  res.send("Reshma  .. Backend is running ")
+});
